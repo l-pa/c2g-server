fd754c06ba18ea29b63383e89abceace8e92ca7d
Lukas Papik <papikdev@gmail.com>
2020-03-25 21:02:25 +0100

    uncaughtException

--- index.js
+++ index.js
@@ -526,3 +526,10 @@ io.on('connection', function (socket) {
 http.listen(port, function () {
   console.log('listening on *:' + port)
 })
+
+
+//Error handler
+process.on('uncaughtException', function (exception) {
+  // handle or ignore error
+  console.log(exception);
+});
